e997a81bdd344cb9647759a3899f6d346da60ef4
Craig Russell <craig.anthony.russell@gmail.com>
2017-04-28 12:12:40 -0500

    Add contractor export endpoint

--- server/plugins/contractorPlugin.js
+++ server/plugins/contractorPlugin.js
@@ -1,23 +1,25 @@
 /**
  * @license
  * Copyright (C) 2017 Phoenix Bright Software, LLC
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 var Boom = require("boom");
 var Joi = require("joi");
+
+var sheeter = require("../lib/sheeter");
 
 var contractorPlugin = {
 
@@ -172,6 +174,52 @@ var contractorPlugin = {
                     return contractor.destroy();
                 }).then(function() {
                     reply();
+                }).catch(function(err) {
+                    return reply(Boom.badRequest(err));
+                });
+            }
+        });
+
+        server.route({
+            method: "GET",
+            path: "/api/v1/contractor/export",
+            config: {
+                cors: {
+                    origin: ["*"]
+                }
+            },
+            handler: function(request, reply) {
+
+                var db = server.plugins["hapi-sequelize"].gigkeeperdb;
+                var models = db.sequelize.models;
+
+                var options = {
+                    where: {
+                        profileId: request.auth.credentials.profileId
+                    },
+                    order: [ "name" ]
+                };
+
+                models.contractor.findAll(options).then(function(contractors) {
+                    var preparedContractors = contractors.map((contractorInstance) => {
+                        var contractor = contractorInstance.get({plain: true});
+
+                        delete contractor.id;
+                        delete contractor.profileId;
+
+                        return contractor;
+                    });
+
+                    var sheets = [
+                        {
+                            name: "Contractors",
+                            data: preparedContractors
+                        }
+                    ];
+
+                    return reply(sheeter.toXLSX(sheets))
+                        .type("application/vnd.ms-excel")
+                        .header("Content-Disposition", "attachment; filename=\"contractors.xlsx\"");
                 }).catch(function(err) {
                     return reply(Boom.badRequest(err));
                 });
