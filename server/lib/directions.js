bcccd178a5f8205d45447b83b9f862fee05999fd
Craig Russell <craig.anthony.russell@gmail.com>
2017-05-04 09:08:18 -0500

    Live reloading works

--- server/lib/directions.js
+++ server/lib/directions.js
@@ -1,17 +1,17 @@
 /**
  * @license
  * Copyright (C) 2017 Phoenix Bright Software, LLC
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -25,7 +25,7 @@ var config = require("../../config/config.js");
 
 /**
  * This service encapsulates Google Directions functionality.
- * 
+ *
  * @return {function}
  */
 module.exports = function() {
@@ -33,11 +33,11 @@ module.exports = function() {
 
     /**
      * Generate a request for the directions from one Google Places ID to another.
-     * 
+     *
      * @param  {string} originPlaceId    A valid Google Places ID
      * @param  {string} destPlaceId    A valid Google Places ID
      * @param  {boolean} alternatives    Request alternative routes? (Default: true)
-     * 
+     *
      * @return {object}  The distance request object
      */
     model.directions = (originPlaceId, destPlaceId, alternatives = true) => {
